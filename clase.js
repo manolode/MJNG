cd6a7f9eac00a1c625bb94cea201eaa1f0a725ab
manolode <manuelnaranjogutierrez@gmail.com>
2023-07-17 20:03:28 +0100

    1

---
+++ clase.js
@@ -0,0 +1,139 @@
+
+// mostrar alerta
+
+// alert("hola");
+
+
+// mostrar en
+// console.log("hola");
+
+
+// sustituir variable
+// var variable=2;
+// variable = 3;
+// console.log(variable)
+
+
+
+// multiplicar
+// var variable=2;
+// variable = 3*3;
+// console.log(variable)
+
+// cambiar el valor de la variable una por otra
+// var a = 2;
+// console.log(a);
+
+// var b = 5;
+// console.log(b)
+
+//  c=a
+//  a=b
+//  b=c
+
+
+// console.log(a) 
+// console.log(b)
+
+
+
+
+// aplicando una variable constante no puedo variar p
+// const p = 3.14;
+// var a = 2;
+// console.log(a);
+
+// var b = 5;
+// console.log(b)
+// // multiplico a por p
+//  c=a*p
+//  a=b
+//  b=c
+
+// console.log(a) 
+// console.log(b)
+
+
+
+// cambio de nombre con string
+// var a = "juan";
+// console.log( "A se llama " +a);
+
+// var b = "jose";
+// console.log("B se llama " + b)
+// // multiplico a por p
+//  c=a
+//  a=b
+//  b=c
+
+// console.log(" ahora A se llama " +a) 
+// console.log(" ahora B se llama " +b)
+
+
+                                    // ACTIVIDAD 2
+
+// parseint   asegura de que sea solo numeros enteros
+// prompt     es como una alerta para que el ususario actue
+                              
+
+// var numero1= parseInt(prompt("Introduce el valor del numero1" , "0"));
+// console.log("valor del numero1 " +  numero1);
+
+// var numero2= parseInt( prompt("introduce el valor de numero2" , "0"));
+// console.log("valor del numero2 "+  numero2);
+
+
+
+// resultado=numero1+numero2;
+// console.log(resultado)
+
+// resultado=numero1-numero2
+// console.log(resultado)
+
+// resultado=numero1*numero2
+// console.log(resultado)
+
+// resultado=numero1/numero2
+// console.log(resultado)
+
+
+                              // ACTIVIDAD  3
+
+// var numero1= parseInt(prompt("Introduce el valor del numero1" , "0"));
+// console.log("valor del numero1 " +  numero1);
+
+// var numero2= parseInt( prompt("introduce el valor de numero2" , "0"));
+// console.log("valor del numero2 " +  numero2);
+
+
+
+// if (numero1==numero2) {
+//     console.log("los numeros son iguales ")
+    
+// }
+// if (numero1>=numero2){
+//     console.log("el numero 1 es superior")
+
+// }
+
+// else {
+//    console.log("el numero2 es superior")
+    
+// }
+
+                             //  ACTIVIDAD 4
+
+
+// var niños= parseInt(prompt("Introduce numero de niños" , "0"));
+// console.log("niños " );
+
+// var niñas= parseInt( prompt("introduce numero de niñas" , "0"));
+// console.log("niñas" );  
+
+// var porcentajeniños =niños*100/(niños+niñas);
+// var porcentajeniñas =100-porcentajeniños;
+
+
+
+//     console.log("hay un " +porcentajeniños+" % de niños" );
+//     console.log("hay un " +porcentajeniñas+" % de niñas");
