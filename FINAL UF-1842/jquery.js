2b88c002b3f82f00b3342ebd1ffafdd9320fa011
manolode <manuelnaranjogutierrez@gmail.com>
2023-08-04 18:19:08 +0100

    04/08

---
+++ FINAL UF-1842/jquery.js
@@ -0,0 +1,20 @@
+
+// si coloco en el head el script esta funcion es necesaria 
+$(document).ready(function (){
+
+
+
+    $(".boton").click(function(){
+        $(".informacion").css
+        ("background-image", "url('imagenes/imagen-curso.jpg')");
+
+        $(".informacion h1,p").css
+        ("color", "#ffff00");
+
+    })
+
+
+ })
+
+
+
